49bffa8566008a04aead9e8802d0552939f4486c
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-07-07 04:37:39 -0400

    css changes

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -17,7 +17,7 @@ const AuthProvider = ({ children }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const user = window.localStorage.getItem("user");
+    const user = auth.currentUser;
     user && setCurrentUser(JSON.parse(user));
   }, []);
 
@@ -26,7 +26,6 @@ const AuthProvider = ({ children }) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then(() => {
         setCurrentUser(auth.currentUser);
-        window.localStorage.setItem("user", JSON.stringify(auth.currentUser));
         // create a user document in firestore
         const userRef = doc(db, "user", auth.currentUser.uid);
         setDoc(
@@ -56,7 +55,6 @@ const AuthProvider = ({ children }) => {
     signInWithEmailAndPassword(auth, email, password)
       .then(() => {
         setCurrentUser(auth.currentUser);
-        window.localStorage.setItem("user", JSON.stringify(auth.currentUser));
         setError(null);
       })
       .catch((err) => {
@@ -65,7 +63,6 @@ const AuthProvider = ({ children }) => {
       });
   };
   const handleSignOut = () => {
-    window.localStorage.clear("user");
     setCurrentUser(null);
     signOut(auth);
     return;
